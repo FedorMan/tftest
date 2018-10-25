29994a14f8570d68b81b9b9c6c58058937e1b404
Fedor <fedor3gr@gmail.com>
2018-10-25 22:03:07 +0400

    add readme and test weather

---
+++ src/main/resources/db/migration/V5__add_test_current_wether.sql
@@ -0,0 +1,6 @@
+insert into  current_weather(city_id, temperature, wind_speed, update_time)
+values(2077746, 34, 23, to_date('2018-10-26', 'YYYY-MM-DD'));
+insert into  current_weather(city_id, temperature, wind_speed, update_time)
+values(2122265, 34, 23, to_date('2018-10-26', 'YYYY-MM-DD'));
+insert into  current_weather(city_id, temperature, wind_speed, update_time)
+values(2123260, 34, 23, to_date('2018-10-26', 'YYYY-MM-DD'));
